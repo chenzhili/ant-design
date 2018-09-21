5fc44bf281e4741b683b2e7459e20ceea4a11ee1
chenzhili <1845882620@qq.com>
2018-09-21 16:54:30 +0800

    change

--- dvaFirst/src/routes/Dashboard/Dashboard.js
+++ dvaFirst/src/routes/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'dva';
 import PropTypes from 'prop-types'
-import { Row, Col, Layout, Menu, Icon, Form, Input } from "antd";
+import { Row, Col, Layout, Menu, Icon, Form, Input,Tree  } from "antd";
 import styles from "./Dashboard.less";
 import logo from "./logo.svg"
 import nprogress from "nprogress";
@@ -13,7 +13,8 @@ import update from 'immutability-helper';
 
 const { Header, Sider, Content, Footer } = Layout;
 const FormItem = Form.Item;
-class Dashboard extends React.Component {
+const TreeNode = Tree.TreeNode;
+class A extends React.Component {
   constructor(props) {
     super(props);
     console.log(this.props);
@@ -115,6 +116,127 @@ class Dashboard extends React.Component {
     )
   }
 }
+
+
+/* 树形结构 */
+const x = 3;
+const y = 2;
+const z = 1;
+const gData = [];
+
+const generateData = (_level, _preKey, _tns) => {
+  const preKey = _preKey || '0';
+  const tns = _tns || gData;
+
+  const children = [];
+  for (let i = 0; i < x; i++) {
+    const key = `${preKey}-${i}`;
+    tns.push({ title: key, key });
+    if (i < y) {
+      children.push(key);
+    }
+  }
+  if (_level < 0) {
+    return tns;
+  }
+  const level = _level - 1;
+  children.forEach((key, index) => {
+    tns[index].children = [];
+    return generateData(level, key, tns[index].children);
+  });
+};
+generateData(z);
+
+console.log(gData);
+class Dashboard extends React.Component {
+  state = {
+    gData,
+    expandedKeys: ['0-0', '0-0-0', '0-0-0-0'],
+  }
+
+  onDragEnter = (info) => {
+    // console.log(info);
+    // expandedKeys 需要受控时设置
+    // this.setState({
+    //   expandedKeys: info.expandedKeys,
+    // });
+  }
+
+  onDrop = (info) => {
+    console.log(info);
+    const dropKey = info.node.props.eventKey;
+    const dragKey = info.dragNode.props.eventKey;
+    const dropPos = info.node.props.pos.split('-');
+    const dropPosition = info.dropPosition - Number(dropPos[dropPos.length - 1]);
+    // console.log("dropKey",dropKey);
+    // console.log("dragKey",dragKey);
+    // console.log("dropPos",dropPos);
+    // console.log("dropPosition",dropPosition);
+    // const dragNodesKeys = info.dragNodesKeys;
+    const loop = (data, key, callback) => {
+      data.forEach((item, index, arr) => {
+        if (item.key === key) {
+          return callback(item, index, arr);
+        }
+        if (item.children) {
+          return loop(item.children, key, callback);
+        }
+      });
+    };
+    const data = [...this.state.gData];
+    let dragObj;
+    loop(data, dragKey, (item, index, arr) => {
+      console.log(data === arr);
+      arr.splice(index, 1);
+      dragObj = item;
+    });
+    console.log(data);
+    if (info.dropToGap) {
+      let ar;
+      let i;
+      loop(data, dropKey, (item, index, arr) => {
+        ar = arr;
+        i = index;
+      });
+      if (dropPosition === -1) {
+        ar.splice(i, 0, dragObj);
+      } else {
+        ar.splice(i + 1, 0, dragObj);
+      }
+    } else {
+      loop(data, dropKey, (item) => {
+        item.children = item.children || [];
+        // where to insert 示例添加到尾部，可以是随意位置
+        item.children.push(dragObj);
+      });
+    }
+    this.setState({
+      gData: data,
+    });
+  }
+
+  render() {
+    const loop = data => data.map((item) => {
+      if (item.children && item.children.length) {
+        return <TreeNode key={item.key} title={item.title}>{loop(item.children)}</TreeNode>;
+      }
+      return <TreeNode key={item.key} title={item.title} />;
+    });
+    return (
+      <Tree
+        className="draggable-tree"
+        // defaultExpandedKeys={this.state.expandedKeys}
+        draggable
+        onDragEnter={this.onDragEnter}
+        onDrop={this.onDrop}
+      >
+        {loop(this.state.gData)}
+      </Tree>
+    );
+  }
+}
+
+
 
 function T(props) {
   return (
