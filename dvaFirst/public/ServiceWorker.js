7fc491f600d384c92b932971305bae713bafd2eb
chenzhili <1845882620@qq.com>
2019-01-04 17:28:42 +0800

    service worker

--- dvaFirst/public/ServiceWorker.js
+++ dvaFirst/public/ServiceWorker.js
@@ -1,36 +1,34 @@
 const CACHE_NAME = 'my-site-cache-v1';
 const urlsToCache = [
   '/',
+  '/index.html',
   '/index.js'
 ];
-
 self.addEventListener('install', function(event) {
   // Perform install steps
   console.log(event);
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 self.addEventListener('fetch', function(event) {
-    console.log(event);
     event.respondWith(
       // 以下方法检视请求，并从服务工作线程所创建的任何缓存中查找缓存的结果。
       caches.match(event.request)
         .then(function(response) {
-          console.log(event.request)
-          console.log(caches)
           // 如果发现匹配的响应，则返回缓存的值
+          console.log(response);
           if (response) {
             return response;
           }
-          return fetch(event.request);
+          return fetch(event.request); 
         }
-      ).catch(err=>{
+      ).catch(err=>{a
           console.log(err);
+          return "error"
       })
     );
 });
